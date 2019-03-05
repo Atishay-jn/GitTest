fb899768e8e75dac9d2cf2b935b15b758dd2d90b
Atishay <Atishay18026@iiitd.ac.in>
2019-03-05 08:47:05 +0530

    just checking

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-var comingdate = new Date("May 1, 2019 00:00:00");
+var comingdate = new Date("May 22, 2019 00:00:00");
 
 var d = document.getElementById('d');
 var h = document.getElementById('h');
